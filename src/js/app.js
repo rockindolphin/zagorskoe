d40096b0465682f0560a2be89f8c1eb57119c0f8
Unknown <rockindolphing@gmail.com>
2018-01-24 17:59:40 +0300

    slider

--- src/js/app.js
+++ src/js/app.js
@@ -26,7 +26,7 @@
 			$(this).hide();
 		});
 
-		//slider
+		//sliders
 		var frontSliderPics = new Swiper('.slider--front-pics .swiper-container', {
 			slidesPerView: 1,
 			on: {
@@ -52,47 +52,23 @@
 			}
 		});
 
-
-		//tabs
-		/*
-		function openTab(data, num){
-			for( var key in data){
-				if( data[key]['visible'] === true ){
-					$(data[key]['content']).removeClass('input__content--visible');
-					data[key]['visible'] = false;					
-				}
-				if( key === num ){
-					$(data[key]['content']).addClass('input__content--visible');
-					data[key]['visible'] = true;
-				}
+		var articlesSlider = new Swiper('.slider--articles .swiper-container', {
+			slidesPerView: 1,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},						
+			on: {
+				init: function(){
+					//console.log('ok');
+				} 
 			}
-		}
+		});		
 
-		$('.input__wrapper--tabs').each(function(){
-			var wrapper = this;
-			wrapper.tabs = {};
-			var checked_num = false;
-			$(wrapper).find('input[data-tab]').each(function(){
-				var num = $(this).attr('data-tab');
-				var content = $(wrapper).find('.input__content[data-tab='+num+']').get(0);
-				wrapper.tabs[num] = { 
-					label: this, 
-					content:content, 
-					num: num, 
-					visible: false 
-				};
-				if( $(this).prop('checked') ){
-					checked_num = num;
-				}
-				$(this).change(function(){
-					openTab(wrapper.tabs, num);
-				});				
-			});
-			if( checked_num ){
-				openTab( wrapper.tabs, checked_num );
-			}
-		});
-*/
 
 	});	
 
